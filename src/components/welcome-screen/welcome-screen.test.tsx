6e5be19115960fadcbcf5332acc7b9a482fc1209
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2020-06-17 15:28:29 +0300

    Добавлены тесты jest и enzyme

---
+++ src/components/welcome-screen/welcome-screen.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import WelcomeScreen from './welcome-screen';
+
+it(`Should WelcomeScreen render correctly`, () => {
+  const tree = renderer
+    .create(<WelcomeScreen
+      errorsCount={3}
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      onWelcomeButtonClick={(): void => {}}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
