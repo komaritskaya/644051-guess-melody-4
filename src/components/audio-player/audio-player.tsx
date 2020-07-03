ccbd4135a09e9cf1a105bac052254a0be418a111
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2020-07-03 04:14:40 +0300

    Аудудиоплеер изменен на FC

--- src/components/audio-player/audio-player.tsx
+++ src/components/audio-player/audio-player.tsx
@@ -1,4 +1,4 @@
-import React, {PureComponent, createRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 interface AudioPlayerProps {
   src: string;
@@ -12,83 +12,50 @@ interface AudioPlayerState {
   isPlaying: boolean;
 }
 
-class AudioPlayer extends PureComponent<AudioPlayerProps, AudioPlayerState> {
-  constructor(props) {
-    super(props);
+const AudioPlayer: React.FC<AudioPlayerProps> = ({src, isPlaying, onPlayButtonClick}) => {
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
 
-    this._audioRef = createRef();
+  const audioRef = useRef<HTMLAudioElement | undefined>();
 
-    this.state = {
-      progress: 0,
-      isLoading: true,
-      isPlaying: props.isPlaying,
-    };
-  }
-
-  componentDidMount() {
-    const {src} = this.props;
-    const audio = this._audioRef.current;
+  useEffect(() => {
+    const audio = audioRef.current;
     audio.src = src;
 
-    audio.oncanplaythrough = () => this.setState({
-      isLoading: false,
-    });
+    const onPlayHandler = () => setLoading(false);
+    audio.addEventListener(`canplaythrough`, onPlayHandler);
 
-    audio.onplay = () => {
-      this.setState({
-        isPlaying: true,
-      });
+    const timeUpdateHandler = () => setProgress(audio.currentTime);
+    audio.addEventListener(`timeupdate`, timeUpdateHandler);
+
+    return () => {
+      audio.removeEventListener(`canplaythrough`, onPlayHandler);
+      audio.removeEventListener(`timeupdate`, timeUpdateHandler);
     };
+  }, [audioRef, setLoading, setProgress, src]);
 
-    audio.onpause = () => this.setState({
-      isPlaying: false,
-    });
-
-    audio.ontimeupdate = () => this.setState({
-      progress: audio.currentTime
-    });
-  }
-
-  componentWillUnmount() {
-    const audio = this._audioRef.current;
-
-    audio.oncanplaythrough = null;
-    audio.onplay = null;
-    audio.onpause = null;
-    audio.ontimeupdate = null;
-    audio.src = ``;
-  }
-
-  render() {
-    const {isLoading, isPlaying} = this.state;
-    const {onPlayButtonClick} = this.props;
-
-    return (
-      <React.Fragment>
-        <button
-          className={`track__button track__button--${isPlaying ? `pause` : `play`}`}
-          type="button"
-          disabled={isLoading}
-          onClick={() => {
-            this.setState({isPlaying: !this.state.isPlaying});
-            onPlayButtonClick();
-          }}
-        />
-        <div className="track__status">
-          <audio ref={this._audioRef} />
-        </div>
-      </React.Fragment>
-    );
-  }
-
-  componentDidUpdate() {
-    const audio = this._audioRef.current;
-    if (this.props.isPlaying) {
+  useEffect(() => {
+    const audio = audioRef.current;
+    if (isPlaying) {
       audio.play();
     } else {
       audio.pause();
     }
-  }
-}
+  }, [audioRef, isPlaying]);
+
+  return (
+    <React.Fragment>
+      <button
+        className={`track__button track__button--${isPlaying ? `pause` : `play`}`}
+        type="button"
+        disabled={loading}
+        onClick={onPlayButtonClick}
+      />
+      <div className="track__status">
+        <audio ref={audioRef} />
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default AudioPlayer;
